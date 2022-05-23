98b096b44862d46c3c4e6223e4c2e8812e14aad4
BenjaminCheng <benjamincheng@vgt-hk.net>
2022-05-23 04:23:10 -0400

    solve 230

---
+++ 230. Kth Smallest Element in a BST/main.go
@@ -0,0 +1,58 @@
+package main
+
+import "fmt"
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func main() {
+	ans := kthSmallest(&TreeNode{
+		Val: 3,
+		Left: &TreeNode{
+			Val: 1,
+			Right: &TreeNode{
+				Val: 2,
+			},
+		},
+		Right: &TreeNode{
+			Val: 4,
+		},
+	}, 1)
+	fmt.Println(ans)
+}
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+var rank int
+var res int
+
+func kthSmallest(root *TreeNode, k int) int {
+	rank = 0
+	res = 0
+	traverse(root, k)
+	return res
+}
+
+func traverse(root *TreeNode, k int) {
+	if root == nil {
+		return
+	}
+
+	kthSmallest(root.Left, k)
+	rank++
+	if rank == k {
+		res = root.Val
+		return
+	}
+	kthSmallest(root.Right, k)
+}
