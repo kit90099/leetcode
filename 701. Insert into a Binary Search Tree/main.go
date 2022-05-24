3a4d1395ab603fa4f1452c37bb61dc6274f10264
BenjaminCheng <benjamincheng@vgt-hk.net>
2022-05-24 03:10:24 -0400

    solve 701

---
+++ 701. Insert into a Binary Search Tree/main.go
@@ -0,0 +1,31 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func insertIntoBST(root *TreeNode, val int) *TreeNode {
+	if root == nil {
+		return &TreeNode{
+			Val: val,
+		}
+	}
+
+	if val < root.Val {
+		root.Left = insertIntoBST(root.Left, val)
+	} else {
+		root.Right = insertIntoBST(root.Right, val)
+	}
+
+	return root
+}
